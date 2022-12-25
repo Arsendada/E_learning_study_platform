187407e9ca5935436a998c7a9f1d8d35f6b4645f
Arsen <arsendada@mail.ru>
2022-12-26 00:35:37 +0300

    created OwnerMixin and OwnerEditMixin

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,5 +1,20 @@
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView,\
+    UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Course
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
 
 class ManageCourseListView(ListView):
