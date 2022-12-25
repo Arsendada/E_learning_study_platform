017c5444347c33bf62c68427b637be37b31a0272
Arsen <arsendada@mail.ru>
2022-12-25 18:18:17 +0300

    add OrderField castom to Module model

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 
 class Subject(models.Model):
@@ -40,6 +41,7 @@ class Module(models.Model):
                                on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['course'])
 
     def __str__(self):
         return self.title
