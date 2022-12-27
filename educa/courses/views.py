671712fd1edea25b34f7beabd4b9d0204b323cf6
Arsen <arsendada@mail.ru>
2022-12-27 03:16:47 +0300

    created ModuleOrderView and ContentOrderVie Add this views to urls.py

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -161,7 +161,18 @@ class ModuleOrderView(CsrfExemptMixin,
                       View):
     def post(self, request):
         for id, order in self.request_json.items():
+            Module.objects.filter(id=id,
+                                   module__course__owner=request.user)\
+                .update(order=order)
+        return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin,
+                      JsonRequestResponseMixin,
+                      View):
+    def post(self, request):
+        for id, order in self.request_json.items():
             Content.objects.filter(id=id,
                                    module__course__owner=request.user)\
                 .update(order=order)
-        return self.render_json_response({'saved':'OK'})
+        return self.render_json_response({'saved': 'OK'})
