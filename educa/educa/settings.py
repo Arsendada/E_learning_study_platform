4fb003546d8b4dcc56b9162888540ea3db717ceb
Arsen <arsendada@mail.ru>
2022-12-29 04:08:20 +0300

    add debug_toolbar to settings.py

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -46,9 +46,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'students.apps.StudentsConfig',
     'embed_video',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
